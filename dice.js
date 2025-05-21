7fcb359fa53777a205b6f6f8403405c2a4345649
simmi25jain <simmi5jain4@gmail.com>
2025-05-21 14:48:42 +0530

    third commit

--- dice.js
+++ dice.js
@@ -1,21 +1,48 @@
-const num=document.querySelectorAll(".num");
-console.log(num);
+const numbers=document.querySelectorAll(".num");
+const diceImg=document.querySelector("img");
+const scoreDisplay=document.querySelector("#score");
 
-const arr=[
-    'dicePhotos/dice_1.png',
-    'dicePhotos/dice_1.png',
-    'dicePhotos/dice_1.png',
-    'dicePhotos/dice_1.png',
-    'dicePhotos/dice_1.png',
-    'dicePhotos/dice_1.png'
-]
+// console.log(num);
+let score=0;
+let selectedNumber;
+let randomNumber;
 
-num.addEventListener("click" , (e)=>{
-    function randomize(arr){
-        const output=[]
-        for(let i=0;i<arr.length;i++){
-            const randomImage= arr[Math.floor(Math.random()*arr.length)].img;  
-        
-        }
-    }
+const diceArr={
+    1: 'dicePhotos/dice_1.png',
+    2: 'dicePhotos/dice_2.png',
+    3: 'dicePhotos/dice_3.png',
+    4: 'dicePhotos/dice_4.png',
+    5: 'dicePhotos/dice_5.png',
+    6: 'dicePhotos/dice_6.png',
+};
+
+numbers.forEach((numb)=>{
+    numb.addEventListener("click",()=>{
+        selectedNumber=numb.innerText;
+        console.log("selectedNumber", selectedNumber);       
+
+    })
 })
+
+diceImg.addEventListener("click",()=>{
+    randomNumber=getARandomNumber();
+    console.log("randomNumber",randomNumber);
+    diceImg.src=diceArr[randomNumber];
+    if(Number(selectedNumber)===Number(randomNumber)){
+        score+=15;
+    }
+    else{
+        score-=5;
+    }
+    scoreDisplay.innerText=score;
+})
+
+function getARandomNumber(){
+    const randomValue=Math.floor(Math.random()*6);
+    if (randomValue===0){
+        return getARandomNumber();
+    }
+    else{
+        return randomValue;
+    }
+}
